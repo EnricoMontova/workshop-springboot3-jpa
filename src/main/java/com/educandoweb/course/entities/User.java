0cb679f02332769ab32c53df18c4f07cb6637994
Enrico <ricomontovanelli@gmail.com>
2025-11-02 10:46:22 -0300

    Comentarios adicionados ao User.java

--- src/main/java/com/educandoweb/course/entities/User.java
+++ src/main/java/com/educandoweb/course/entities/User.java
@@ -14,27 +14,34 @@ import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
+// Marca essa entidade do JPA (Será uma tabela no banco)
 @Entity
-@Table(name="tb_user")
+@Table(name="tb_user") // Define o nome da tabela
 public class User implements Serializable {
 	
 	private static final long serialVersionUID = 1L; 
 	
+	// Define o atributo id como chave primária
 	@Id
-	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@GeneratedValue(strategy=GenerationType.IDENTITY) // Gera ids automaticamente
 	private Long id;
+	
+	// Colunas da tabela
 	private String name;
 	private String email;
 	private String phone;
 	private String password;
 	
-	@JsonIgnore
-	@OneToMany(mappedBy="client")
+	// Um usuários pode ter vários pedidos (1:N)
+	@JsonIgnore // Evita loop infinito quando JSON tenta converter os pedidos e os usuários ao mesmo tempo
+	@OneToMany(mappedBy="client") // Client é o nome do atributo que mapeia o usuário dentro da classe Order
 	private List<Order> orders = new ArrayList<>();
 	
+	// Construtor padrão obrigatório para JPA
 	public User() {
 	}
-
+	
+	// Construtor com argumentos (usado para criação manual de objetos)
 	public User(Long id, String name, String email, String phone, String password) {
 		this.id = id;
 		this.name = name;
@@ -42,7 +49,8 @@ public class User implements Serializable {
 		this.phone = phone;
 		this.password = password;
 	}
-
+	
+	// Getters e Setters (permitem acessar e alterar atributos)
 	public Long getId() {
 		return id;
 	}
@@ -86,7 +94,8 @@ public class User implements Serializable {
 	public List<Order> getOrders() {
 		return orders;
 	}
-
+	
+	// Métodos para comparar objetos (Importante para JPA)
 	@Override
 	public int hashCode() {
 		return Objects.hash(id);
